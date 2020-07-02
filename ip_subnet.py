df85deb4aa06a13a7475b55b5899181995eb88c6
Bashorun97 <bashorun.emma@gmail.com>
2020-07-02 23:43:45 +0100

    Fixed bugs on subnetworks and host creation

--- ip_subnet.py
+++ ip_subnet.py
@@ -100,17 +100,33 @@ class CreateSubnetwork(Subnet):
     #sub_quantity = args_map['subnetworks_info']
     pass
 
-  def generate_base(self, sub_quantity, parent_addr):
-    network_id, cidr = cidr_spliter(parent_addr)
-    sub_quantity = math.log2(sub_quantity)
-    extended_network_prefix = [network_id, str(math.floor(sum([cidr, sub_quantity])))]
-    new_cidr = '/'.join(extended_network_prefix)
-    child_addr, parent_addr = map(prefix_to_decimal_converter, (new_cidr, parent_addr))
-    return parent_addr, child_addr
+  def generate_metadata(self, **kwargs):
+    network_id, cidr = cidr_spliter(kwargs['parent_addr'])
+    
+    if kwargs.__contains__('sub_quantity'):
+      added_bits = math.log2(kwargs['sub_quantity'])
+      sub = str(math.floor(sum([cidr, added_bits])))
+      extended_network_address = [network_id, sub]
+
+    else:
+      # Host quantity
+      quantity = 32 - math.log2(kwargs['host_quantity'])
+      extended_network_address = [network_id, str(math.floor(quantity))]
+
+    # new_cidr depending on the outcome of preceding control structure
+    new_cidr = '/'.join(extended_network_address)
+    child_addr, parent_addr = map(prefix_to_decimal_converter, (new_cidr, kwargs['parent_addr']))
+    parent_net = self.get_network_id(parent_addr[0], parent_addr[1])
+    child_net = self.get_network_id(child_addr[0], child_addr[1])
+    
+    # extended_subnet_bits = [bin(octet).strip('0b') for octet in extended_network_address]
+
+    network_metadata = {'parent_addr':parent_net, 'child_addr':child_net}
+    return network_metadata
 
   def generate_addresses(self, args):
-    prev_addresses = self.subnet_ip_addresses(args)
-    return prev_addresses
+    addresses = self.subnet_ip_addresses(args)
+    return addresses
 
 
 def argparse_unpacker(parse_object=None):
@@ -120,7 +136,8 @@ def argparse_unpacker(parse_object=None):
 
   network_prefix = parsed.network_prefix
   ip_and_subnetmask = parsed.ip_and_subnetmask
-  subnetworks_info = parsed.subnetworks_info
+  create_subnets = parsed.create_subnets
+  create_hosts = parsed.create_hosts
   
   if network_prefix is not None:
     network_id_and_subnet = network_prefix
@@ -130,11 +147,22 @@ def argparse_unpacker(parse_object=None):
     network_id_and_subnet = tuple(ip_and_subnetmask)
     keyword_map['ip_and_subnetmask'] = network_id_and_subnet
 
-  if subnetworks_info is not None:
-    subnet_info = tuple(subnetworks_info)
-    keyword_map['subnetworks_info'] = subnet_info
+  if create_subnets is not None:
+    subnet_info = tuple(create_subnets)
+    keyword_map['create_subnets'] = subnet_info
+
+  if create_hosts is not None:
+    host_info = tuple(create_hosts)
+    keyword_map['create_hosts'] = host_info
+
 
   return keyword_map
+
+
+def str_serializer(arg):
+  arr = map(str, arg)
+  arr = '.'.join(arr)
+  return arr
 
 
 if __name__ == '__main__':
@@ -143,13 +171,13 @@ if __name__ == '__main__':
   create = CreateSubnetwork()
 
   parse_object = parser_child.parser
-
+  
   # Unpacks keyword_map into args_map
   args_map = argparse_unpacker(parse_object)
-  network_prefix = args_map['network_prefix']
 
   # Gracefully handles the exception of passing one flag
   try:
+    network_prefix = args_map['network_prefix']
     net, submask = prefix_to_decimal_converter(network_prefix)
   except KeyError:
     net, submask = args_map['ip_and_subnetmask']
@@ -162,17 +190,42 @@ if __name__ == '__main__':
     #====================
     #Creating Subnetworks
     #====================
+    if args_map.__contains__('create_subnets'):
 
-    sub_quantity = args_map['subnetworks_info'][0][0]
-    # Generates a tuple of parent and child address
-    parent_child = create.generate_base(sub_quantity, network_prefix)
-    parent, child  = parent_child
-    parent_ip = create.generate_addresses(parent[1])
-    child_ip = create.generate_addresses(child[1])
-    print(f'Base Network ID of {parent[0]} => {parent_ip} Hosts')
-    print(f'{sub_quantity} newly created subnetworks with {child_ip} Hosts ')
+      sub_quantity = args_map['create_subnets'][0]
+
+      # Generates networks metadata. Contains base address, new address and subnet bits
+      metadata = create.generate_metadata(sub_quantity=sub_quantity, parent_addr=network_prefix)
+      parent = metadata['parent_addr']
+      child = metadata['child_addr']
+      #subnet = metadata['subnet_bits']
+      
+      parent_mask = str_serializer(parent['subnet_mask'])
+      child_mask = str_serializer(child['subnet_mask'])
+      parent_hosts = create.generate_addresses(parent_mask)
+      child_hosts = create.generate_addresses(child_mask)
+
+      print(f"Base Network ID of {parent['network_id']} => {parent_hosts} Hosts.")
+      print(f"Network ID => {parent['network_id']}")
+      print(f'{sub_quantity} newly created subnetworks defined with {child_hosts} Hosts ')
+
+    # Then args_map contains create_hosts
+    else:
+      host_quantity = args_map['create_hosts'][0]
+      metadata = create.generate_metadata(host_quantity=host_quantity, parent_addr=network_prefix)
+      parent = metadata['parent_addr']
+      child = metadata['child_addr']
+      
+      parent_mask = str_serializer(parent['subnet_mask'])
+      child_mask = str_serializer(child['subnet_mask'])
+
+      parent_ip = create.generate_addresses(parent_mask)
+      child_ip = create.generate_addresses(child_mask)
+      print(f"Base Network ID of {parent['network_id']} => {parent_ip} Hosts")
+      print(f'{child_ip} newly created hosts defined within xx Subnetworks')
+
   else:
     print(ip_address_class)
-    print(f"Base Network ID =>  {network_info['network_id']}")
-    print(f'Number of available hosts and IP Addresses => {available_ip}')
+    print(f"Base Network ID => {network_info['network_id']}")
+    print(f'Number of available hosts or IP Addresses => {available_ip}')
 
